92779376931b611003612fb5470c192e96635d23
affan <affan.habib44@gmail.com>
2024-08-18 12:27:31 +0600

    removed canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 // App.js
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import EditorComponent from './components/EditorComponent';
 import ColorPicker from './components/ColorPicker';
 import PageControls from './components/PageControls';
-import PaintApp from './components/Canvas';
 
 const App = () => {
-  const [showCanvas, setShowCanvas] = useState(false);
   const displayColorPicker = useSelector((state) => state.pages.displayColorPicker);
-
   return (
     <div className="relative body-class">
-      <button onClick={() => setShowCanvas(true)}>Show Canvas</button>
-      <button onClick={() => setShowCanvas(false)}>Show Editor</button>
       {displayColorPicker && (
         <ColorPicker />
       )}
       <PageControls />
-      {showCanvas ? <PaintApp /> : <EditorComponent/>}
+      <EditorComponent />
     </div>
   );
 };
